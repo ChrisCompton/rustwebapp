8215ba1e36f96defffd582eebf638e94698066f7
Jake Scott <jake.net@gmail.com>
2015-07-03 16:21:20 +1200

    update to show POWERED_BY env variable

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate iron;
 
+use std::env;
 use iron::prelude::*;
 use iron::status;
 
@@ -8,8 +9,17 @@ fn main() {
 	println!("runnning iron...");
 
     Iron::new(|_: &mut Request| {
+		
+		let powered_by:String = match env::var("POWERED_BY") {
+		    Ok(val) => val,
+		    Err(_) => "Deis".to_string()
+		};
+
     	println!("got request");
-        Ok(Response::with((status::Ok, "Hello world!")))
+
+    	let message = format!("Powered by {}", powered_by);
+        Ok(Response::with((status::Ok, message)))
+
     }).http("0.0.0.0:8080").unwrap();
 
 }
