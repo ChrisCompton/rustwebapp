25168102769255e3dd938ead726135105ff9f150
Jake Scott <jake.net@gmail.com>
2015-07-05 21:23:42 +1200

    add router

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,56 @@
 extern crate iron;
+extern crate persistent;
+extern crate router;
 
 use std::env;
+use std::net::*;
 use iron::prelude::*;
 use iron::status;
+use iron::typemap::Key;
+use router::{Router};
+use persistent::Write;
 
-fn main() {
+#[derive(Copy, Clone)]
+pub struct HitCounter;
 
-    println!("runnning iron...");
+impl Key for HitCounter { type Value = usize; }
+
+fn index(_: &mut Request) -> IronResult<Response> {
+    let powered_by:String = match env::var("POWERED_BY") {
+        Ok(val) => val,
+        Err(_) => "Iron".to_string()
+    };
+    let message = format!("Powered by: {}", powered_by);
+    Ok(Response::with((status::Ok, message)))
+}
+
+fn posts(req: &mut Request) -> IronResult<Response> {
+    let ref post_id = req.extensions.get::<Router>().unwrap().find("post_id").unwrap_or("none");
+    Ok(Response::with((status::Ok, "PostId: {}", *post_id)))
+}
+
+fn hits(req: &mut Request) -> IronResult<Response> {
+    let mutex = req.get::<Write<HitCounter>>().unwrap();
+    let mut count = mutex.lock().unwrap();
+    *count += 1;
+    Ok(Response::with((status::Ok, format!("Hits: {}", *count))))
+}
+
+fn main() {    
+    let ip = Ipv4Addr::new(0, 0, 0, 0);
+    let port = 8080;
+    let address = SocketAddrV4::new(ip, port);
     
-    Iron::new(|_: &mut Request| {
-        
-        let powered_by:String = match env::var("POWERED_BY") {
-            Ok(val) => val,
-            Err(_) => "Deis".to_string()
-        };
-        println!("got request..");
-        
-        let message = format!("Powered by {}", powered_by);
-        Ok(Response::with((status::Ok, message)))
+    println!("listening on http://{}", address);
 
-    }).http("0.0.0.0:8080").unwrap();
+    let mut router = Router::new();
 
+    router.get("/", index);
+    router.get("/posts/:post_id", posts);
+    router.get("/hits", hits);
+
+    let mut middleware = Chain::new(router);
+    middleware.link(Write::<HitCounter>::both(0));
+
+    Iron::new(middleware).http(address).unwrap();
 }
