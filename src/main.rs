05bd0e0d18850d95266a7fa63fe673ff402fa8e7
Jake Scott <jake.net@gmail.com>
2015-07-07 09:03:20 +1200

    connection pool works!

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,14 @@ extern crate r2d2_postgres;
 
 use std::env;
 use std::net::*;
-use std::sync::Arc;
-use std::thread;
-use std::default::Default;
 
 use iron::prelude::*;
 use iron::status;
 use iron::typemap::Key;
-use router::{Router};
-use persistent::Write;
+use router::Router;
+use persistent::{Write,Read};
 
-use postgres::{Connection, SslMode};
-use r2d2::{Pool, PooledConnection, Config};
+use r2d2::{Pool, PooledConnection};
 use r2d2_postgres::{PostgresConnectionManager};
 
 macro_rules! try {
@@ -30,12 +26,16 @@ macro_rules! try {
     )
 }
 
+pub type PostgresPool = Pool<PostgresConnectionManager>;
+pub type PostgresPooledConnection = PooledConnection<PostgresConnectionManager>;
+
 #[derive(Copy, Clone)]
 pub struct HitCounter;
 impl Key for HitCounter { type Value = usize; }
 
-pub type PostgresPool = Pool<PostgresConnectionManager>;
-pub type PostgresPooledConnection = PooledConnection<PostgresConnectionManager>;
+pub struct AppDb;
+impl ::iron::typemap::Key for AppDb { type Value = PostgresPool; }
+
 
 fn index(_: &mut Request) -> IronResult<Response> {
     let powered_by:String = match env::var("POWERED_BY") {
@@ -56,27 +56,29 @@ fn hits(req: &mut Request) -> IronResult<Response> {
     let mut count = mutex.lock().unwrap();
     *count += 1;
     Ok(Response::with((status::Ok, format!("Hits: {}", *count))))
+} 
+
+fn database(req: &mut Request) -> IronResult<Response> {
+    let pool = req.get::<Read<AppDb>>().unwrap();
+    let conn = pool.get().unwrap();
+    let stmt = try!(conn.prepare("SELECT id FROM messages;"));
+    for row in try!(stmt.query(&[])) {
+        let id: i32 = row.get(0);
+        println!("id: {}", id);
+    }
+    Ok(Response::with((status::Ok, format!("Db: {}", "ok"))))
 }
 
 pub fn setup(cn_str: &str, pool_size: u32) -> PostgresPool {
     let manager = ::r2d2_postgres::PostgresConnectionManager::new(cn_str, ::postgres::SslMode::None).unwrap();
-    
-    let config = ::r2d2::Config::builder()
-        .pool_size(pool_size)
-        .build();    
-
+    let config = ::r2d2::Config::builder().pool_size(pool_size).build();
     ::r2d2::Pool::new(config, manager).unwrap()
 }
 
 fn main() {
-
     println!("connecting to postgres");
 
-    //let config = r2d2::Config::default();
-    //let manager:PostgresConnectionManager = PostgresConnectionManager::new("postgres://dbuser:dbpass@172.17.0.18:5432/test", SslMode::None).unwrap();
-    //let pool = Arc::new(r2d2::Pool::new(config, manager).unwrap());
-
-    let pool:PostgresPool = setup("postgres://dbuser:dbpass@172.17.0.18:5432/test", 6);
+    let pool:PostgresPool = setup("postgres://dbuser:dbpass@localhost:5432/test", 6);
     let conn:PostgresPooledConnection = pool.get().unwrap();
 
     try!(conn.execute("DROP TABLE IF EXISTS messages;", &[]));
@@ -103,9 +105,11 @@ fn main() {
     router.get("/", index);
     router.get("/posts/:post_id", posts);
     router.get("/hits", hits);
+    router.get("/database", database);
 
     let mut middleware = Chain::new(router);
     middleware.link(Write::<HitCounter>::both(0));
+    middleware.link(Read::<AppDb>::both(pool));    
 
     Iron::new(middleware).http(address).unwrap();
 }
